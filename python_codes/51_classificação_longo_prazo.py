ad178779a8febdb737f3437136e99b2179143724
Edu Bernardino <edu.bernardino6@hotmail.com>
2024-10-23 20:19:17 -0300

    classificacao 1 commit

--- python_codes/51_classificação_longo_prazo.py
+++ python_codes/51_classificação_longo_prazo.py
@@ -13,7 +13,12 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler,OneHotEncoder,MinMaxScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,cross_val_score,cross_val_predict,KFold
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold,RepeatedStratifiedKFold,KFold,RepeatedKFold,train_test_split,GridSearchCV
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+import pickle as pkl
 
 """CLASSIFICAR EMPRESAS PARA INVESTIMENTO A LONGO PRAZO"""
 dataset = pd.read_excel('database//BD Completo.xlsx')
@@ -23,8 +28,8 @@ plt.style.use('Solarize_Light2')
 """-------------------------------- TRATAR VALORES FALTANTES --------------------------------"""
 
 sns.heatmap(dataset.isnull())
-pd.set_option('display.max_rows', None)
-pd.reset_option('display.max_rows')
+
+
 dataset.isnull().sum()
 dataset.columns = dataset.columns.str.strip().str.upper()
 
@@ -34,6 +39,9 @@ for i in dataset.columns:
 
 
 dataset.isnull().sum()
+sns.heatmap(dataset.isnull())
+
+
 
 #SELECIONAR CATEGORIGOS E NUMÉRICOS
 cat_attribs = dataset.select_dtypes(include=['object']).columns.tolist()
@@ -174,7 +182,7 @@ plt.xticks()
 
 
 
-#DESCRIBE
+#DESCRIBE DATA
 dataset.describe()
 dataset[dataset['LPA DESCONCTADO'] > 160] 
 
@@ -214,6 +222,7 @@ sns.heatmap(correlacao[correlacao.abs() > 0.5], annot=True, cmap='coolwarm', cba
 
 #ONE HOT ENCODER E VARIAVEIS DUMMIES
 dataset_original = dataset.copy()
+dataset_original['SITUAÇÃO'].value_counts()
 
 cat_attribs = dataset.select_dtypes(include=['object']).columns.tolist()
 num_attribs = dataset.select_dtypes(include=['number']).columns.tolist()
@@ -234,8 +243,124 @@ dataset = dataset.drop(['SEGMENTO','CATEGORIA','SITUAÇÃO','EMPRESA'],axis=1)
 #CONCATENAÇÃO DE DATAFRAMES
 dataset.index
 X_cat.index
-
-
 dataset = pd.concat([dataset,X_cat],axis=1)
 
+
+#NORMALIZAÇÃO
+scaler = MinMaxScaler()
+dataset.columns = dataset.columns.astype(str)
+dataset_norma = scaler.fit_transform(dataset)
+
+X = dataset_norma.copy()
+
+
+
+#VALIDAÇÃO CRUZADA
+#--------------------------------------------MODELOS--------------------------------------------
+rnd_class = RandomForestClassifier(random_state=42,verbose=1)
+mlp_class = MLPClassifier(hidden_layer_sizes=(175,175),verbose=1,random_state=42)
+resultados_random_forest = []
+resultados_random_forest_estratificado = []
+resultados_rede_neural_estratificado = []
+
+#-----------------------------------------------------------------------------------------------
+
+#RANDOM FOREST#
+rskfold = RepeatedStratifiedKFold(n_splits=10, n_repeats=30, random_state=2)
+scores = cross_val_score(rnd_class, X, y, cv=rskfold)
+print(f"Desempenho Médio Random Forest Estratificado: {np.mean(scores)}")
+resultados_random_forest_estratificado.append(scores)
+
+
+rkfold = RepeatedKFold(n_splits=10,n_repeats=30,random_state=42)
+scores = cross_val_score(rnd_class, X, y, cv=rkfold)
+resultados_random_forest.append(scores)
+
+print(f"Desempenho Médio Random Forest Não-Estratificado: {np.mean(scores)}")
+#-----------------------------------------------------------------------------------------------
+#REDE NEURAL
+scores = cross_val_score(mlp_class,X,y,cv=rskfold)
+print(f"Desempenho Médio Multilayer Perceptron Estratificado: {np.mean(scores)}")
+resultados_rede_neural_estratificado.append(scores)
+
+
+np.mean(resultados_random_forest_estratificado),np.mean(resultados_rede_neural_estratificado)
+
+
+
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+random_forest = RandomForestClassifier()
+random_forest.fit(X_train,y_train)
+previsoes = random_forest.predict(X_test)
+
+accuracy_score(y_test,previsoes)
+
+random_forest.classes_
+cm = confusion_matrix(y_test,previsoes)
+sns.heatmap(cm,annot=True)
+print(classification_report(y_test,previsoes))
+
+
+#recall - identifica corretamente
+#precision - classificar corretamente
+
+#FEATURE IMPORTANCE
+
+
+for nome,importancia in zip(dataset.columns,random_forest.feature_importances_):
+    print(f'{nome} :  {importancia} ')
+
+
+
+
+features = dataset.columns
+importancias = random_forest.feature_importances_
+indices = np.argsort(importancias)
+
+plt.figure(figsize=(40,50))
+plt.title('Feature Importance')
+plt.barh(range(len(indices)),importancias[indices],align='center')
+plt.yticks(range(len(indices)),[features[i] for i in indices])
+plt.xlabel('Importances')
+
+
+
+#TUNING DE HIPERPARAMETROS
+
+parametros = {'criterion':['gini','entropy'],'min_samples_split':[2,4,6,8],
+            'n_estimators':[50,100,150,200]}
+
+
+grid_searcher = GridSearchCV(estimator=RandomForestClassifier(n_jobs=-1,verbose=1),
+                param_grid=parametros)
+
+
+grid_searcher.fit(X,y)
+best_params = grid_searcher.best_params_
+best_score = grid_searcher.best_score_
+
+random_forest_best = grid_searcher.best_estimator_
+
+
+
+
+
+#TESTE DE VOTING CLASSIFIER - ENSEMBLE DE MODELOS
+
+mlp_class = MLPClassifier(max_iter=1000)
+named_estimators = [
+    ("random_forest_clf", random_forest_best),
+    ("mlp_clf", mlp_class),
+]
+
+from sklearn.ensemble import VotingClassifier
+
+voting_clf = VotingClassifier(named_estimators)  
+voting_clf.fit(X,y)
+
+
+previsoes = voting_clf.predict(X_test)
+accuracy_score(y_test,previsoes)
+#OVERFITTING
 
